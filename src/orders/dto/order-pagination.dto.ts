81548930b9c4782f285c4e3e1e0167548e7772eb
EricMI <98968734+EricMI-Dapper@users.noreply.github.com>
2025-10-22 19:46:22 -0600

    feat: implement pagination and exception handling for order retrieval

---
+++ src/orders/dto/order-pagination.dto.ts
@@ -0,0 +1,12 @@
+import { IsEnum, IsOptional } from "class-validator";
+import { PaginationDto } from "src/common";
+import { OrderStatusList } from "../enum/order.enum";
+import { OrderStatus } from "generated/prisma/enums";
+
+export class OrderPaginationDto extends PaginationDto {
+    @IsOptional()
+    @IsEnum(OrderStatusList, {
+        message: `status must be a valid enum value: ${Object.values(OrderStatusList).join(', ')}`
+    })
+    status: OrderStatus;
+}
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -2,6 +2,8 @@ import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { PrismaClient } from '../../generated/prisma/client';
 import { RpcException } from '@nestjs/microservices';
+import { OrderPaginationDto } from './dto/order-pagination.dto';
+import { last } from 'rxjs';
 
 @Injectable()
 export class OrdersService extends PrismaClient implements OnModuleInit {
@@ -18,8 +20,32 @@ export class OrdersService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findAll(orderPaginationDto: OrderPaginationDto) {
+    console.log('entando a findall');
+    
+    const totalPages = await this.order.count({
+      where: {
+        status: orderPaginationDto.status,
+      },
+    });
+
+    const currentPage = orderPaginationDto.page || 1;
+    const perPage = orderPaginationDto.limit || 10;
+
+    return {
+      data: await this.order.findMany({
+        skip: (currentPage - 1) * perPage,
+        take: perPage,
+        where: {
+          status: orderPaginationDto.status,
+        }
+      }),
+      meta: {
+        total: totalPages,
+        page: currentPage,
+        lastPage: Math.ceil(totalPages / perPage),
+      }
+    }
   }
 
   async findOne(id: string) {
